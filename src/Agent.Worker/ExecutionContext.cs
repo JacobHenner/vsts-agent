5eb9074f971971ecc77f9d24525f75a2ac1be70f
Tingluo Huang <tihuang@microsoft.com>
2018-03-22 18:02:06 -0400

    c

--- src/Agent.Worker/ExecutionContext.cs
+++ src/Agent.Worker/ExecutionContext.cs
@@ -489,7 +489,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
                 }
             }
 
-            _jobServerQueue.QueueWebConsoleLine(msg);
+            _jobServerQueue.QueueWebConsoleLine(_record.Id, msg);
             return totalLines;
         }
 
